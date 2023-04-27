7124d1e3dd23902d48cf4e6ac9e767289354371a
Marian <marianmoh21@gmail.com>
2023-04-27 14:59:22 +0300

    Added underlines

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("partialsLoaded", () => {
+   import('./burger-menu.js');
+   import('./showtext.js');
+   import('./add-underlines.js');
+});
---
+++ scripts/menu.js
@@ -0,0 +1,4 @@
+document.addEventListener("partialsLoaded", () => {
+   import('./burger-menu.js');
+   import('./add-underlines.js');
+});
