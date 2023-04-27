d2d6d6184e910a42ad4438e90919596193dac004
Marian <marianmoh21@gmail.com>
2023-04-27 23:15:33 +0300

    added two carousels in menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,6 @@
 document.addEventListener("partialsLoaded", () => {
    import('./burger-menu.js');
+   import('./bar-carousel.js');
+   import('./food-carousel.js');
    import('./add-underlines.js');
 });
