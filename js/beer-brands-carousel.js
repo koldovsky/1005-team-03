3478076d081534ddc685ec242f94611e6f0a38df
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2023-05-01 01:50:30 +0300

    beer-brands-carousel in progress. JS added

--- js/beer-brands-carousel.js
+++ js/beer-brands-carousel.js
@@ -1,51 +1,91 @@
-const carouselContainer = document.querySelector('.beer-brands-carousel__images');
-const prevButton = document.querySelector('.beer-brands-carousel__images-button-prev');
-const nextButton = document.querySelector('.beer-brands-carousel__images-button-next');
-const items = document.querySelectorAll('.beer-brands-carousel__images-container');
+const slides = [`
+<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_miller.svg" alt="Miller">
+</div>`,
+`<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_hoegaarden.svg" alt="Hoegaarden">
+</div>`,
+`<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_corona.svg" alt="Corona">
+</div>`,
+`<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_heineken.svg" alt="Heineken">
+</div>`,
+`<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_carlsberg.svg" alt="Carlsberg">
+</div>`,
+`<div class="beer-brands-carousel__image-block">
+    <img class="beer-brands-carousel__image" src="/images/carousel_logo_budweiser.svg" alt="Budweiser">
+</div>   
+`];
 
-const beerBrands = [
-`<div class="miller-brand">
-<img src="/images/carousel_logo_miller.svg" alt="Miller">
-</div>
-<div class="hoegaarden-brand">
-    <img src="/images/carousel_logo_hoegaarden.svg" alt="Hoegaarden">
-</div>
-<div class="carlsberg-brand">
-    <img src="/images/carousel_logo_carlsberg.svg" alt="Carlsberg">
-</div>
-<div class="budweiser-brand">
-    <img src="/images/carousel_logo_budweiser.svg" alt="Budweiser">
-</div>
-<div class="heineken-brand">
-    <img src="/images/carousel_logo_heineken.svg" alt="Heineken">
-</div>
-<div class="corona-brand">
-    <img src="/images/carousel_logo_corona.svg" alt="Corona">
-</div>`
-]
+let currentSlide = 0;
 
-let currentIndex = 0;
-items[currentIndex].classList.add('active');
 
-function showItem(index) {
-  if (index < 0) {
-    index = items.length - 1;
-  } else if (index >= items.length) {
-    index = 0;
-  }
-  items[currentIndex].classList.remove('active');
-  items[index].classList.add('active');
-  currentIndex = index;
+function showCurrentSlide() {
+    const slideContainer = document.querySelector('.beer-brands-carousel__images-container');
+    slideContainer.innerHTML = slides[currentSlide];
+    switch (true) {
+        case window.innerWidth >= 479:
+            const secondSlideIdx = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+            slideContainer.innerHTML += slides[secondSlideIdx];
+            if (window.innerWidth < 767) {
+                break;
+            }
+        case window.innerWidth >= 767:
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
+            if (window.innerWidth < 991) {
+                break;
+            }
+        case window.innerWidth >= 991:
+            const fourthSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
+            slideContainer.innerHTML += slides[fourthSlideIdx];
+            const fifthSlideIdx = fourthSlideIdx + 1 >= slides.length ? 0 : fourthSlideIdx + 1;
+            slideContainer.innerHTML += slides[fifthSlideIdx];
+            break;
+    }
 }
 
-prevButton.addEventListener('click', () => {
-  showItem(currentIndex - 1);
-});
 
-nextButton.addEventListener('click', () => {
-  showItem(currentIndex + 1);
-});
+function prevSlide() {
+    currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1;
+    showCurrentSlide();
+}
 
-setInterval(() => {
-  showItem(currentIndex + 1);
-}, 5000);
+function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+    showCurrentSlide();
+}
+
+nextSlide();
+
+const btnNext = document.querySelector('.beer-brands-carousel__images-button-prev');
+btnNext.addEventListener('click', prevSlide);
+
+const btnPrev = document.querySelector('.beer-brands-carousel__images-button-next');
+btnPrev.addEventListener('click', nextSlide);
+
+window.addEventListener('resize', showCurrentSlide);
+
+
+// function showCurrentSlide() {
+//     const slideContainer = document.querySelector('.beer-brands-carousel__images-container');
+//     slideContainer.innerHTML = slides[currentSlide];
+//     switch (true) {
+//         case window.innerWidth >= 479:
+//             const secondSlideIdx = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+//             slideContainer.innerHTML += slides[secondSlideIdx];
+//             break;
+//         case window.innerWidth >= 767:
+//             const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+//             slideContainer.innerHTML += slides[thirdSlideIdx];
+//             break;
+//         case window.innerWidth >= 991:
+//             const fourthSlideIdx = thirdSlideIdx + 1 >= slides.length ? 0 : thirdSlideIdx + 1;
+//             slideContainer.innerHTML += slides[fourthSlideIdx];
+//             const fifthSlideIdx = fourthSlideIdx + 1 >= slides.length ? 0 : fourthSlideIdx + 1;
+//             slideContainer.innerHTML += slides[fifthSlideIdx];
+//             break;
+//     }
+// }
